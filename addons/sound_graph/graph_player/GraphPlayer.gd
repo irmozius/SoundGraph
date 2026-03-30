f0d481b3860318ec42a07fc9ad3bf079ea74f8be
irmoz <kyz1610@gmail.com>
2026-03-30 16:37:28 +0100

    Can change root node without error

--- addons/sound_graph/graph_player/GraphPlayer.gd
+++ addons/sound_graph/graph_player/GraphPlayer.gd
@@ -3,13 +3,20 @@ class_name GraphPlayer extends Node
 
 @export_tool_button("Test Sound", "AudioStreamPlayer") var test_button : Callable = play
 
-@export var root_node : Node
+@export var root_node : Node:
+	set(n):
+		root_node = n
+		set_graph_root_node()
+
 @export var graph : SoundGraph:
 	set(g):
 		graph = g
-		for res : PlayerResource in graph.graph:
-			res.root_node = root_node
+		set_graph_root_node()
 
 func play():
 	for res : PlayerResource in graph.graph:
 		res.execute()
+
+func set_graph_root_node():
+	for res : PlayerResource in graph.graph:
+		res.root_node = root_node
